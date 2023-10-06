fe1252a10b6958301218aa61cbc3a7a73bd22ab8
niyaz <niyazbek1134@gmail.com>
2023-10-07 01:19:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 
-data = {
-    "x" : [i for  i in range (1,10)],
-    "y" : [i * i for i in range(1,10)]
+data= {
+    "x" : [i for i in range (1,10)],
+    "y" : [i*i for i in range (1,10)]
 }
-plt.plot(
+
+plt.scatter(
     data["x"],
     data["y"],
+    marker="o"
 )
 
 plt.show()
