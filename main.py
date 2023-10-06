67a62913f2e81b7add4ceaecda99be680515dcc9
niyaz <97833998+niyazzz707@users.noreply.github.com>
2023-10-07 01:25:44 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import numpy as np
+import Adafruit_DHT
 
-data= {
-    "x" : [i for i in range (1,10)],
-    "y" : [i*i for i in range (1,10)]
-}
+sensor=Adafruit_DHT.DHT11
+pin=20
 
-plt.scatter(
-    data["x"],
-    data["y"],
-    marker="o"
-)
+fig=plt.figure()
+ax=plt.axes(xlim=(0,30), ylim=(15,45))
+max_points=30
+line, = ax.plot(np.arange(max_points), np.ones(max_points, dtype=np.float) * np.nan, lw=1, c='blue', marker='d', ms=2)
 
-plt.show()
+def init():
+    return line
+
+h,t = Adafruit_DHT.read_retry(sensor,pin)
+
+def animate(i):
+    h,t=Adafruit_DHT.read_retry(sensor,pin)
+    y=t
+    old_y=line.get_ydata()
+    new_y=np.r_[old_y[1:], y]
+    line.set_ydata(new_y)
+    return line,
